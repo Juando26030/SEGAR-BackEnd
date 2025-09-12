3414cd69f96bc87f360f1ebe04976c3275947aa0
Lucas R <lucasrivera6021@gmail.com>
2025-09-11 20:00:57 -0500

    Migracion completa falta el apigetway

--- segar-backend/src/main/java/com/segar/backend/documentos/DocumentosModule.java
+++ segar-backend/src/main/java/com/segar/backend/documentos/DocumentosModule.java
@@ -1,8 +1,10 @@
 package com.segar.backend.documentos;
 
+import com.segar.backend.shared.SharedModule;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.segar.backend.documentos.infrastructure")
 @EnableTransactionManagement
 @ConfigurationPropertiesScan
+@Import(SharedModule.class)
 public class DocumentosModule {
 
     public static final String MODULE_NAME = "documentos";
--- segar-backend/src/main/java/com/segar/backend/shared/SharedModule.java
+++ segar-backend/src/main/java/com/segar/backend/shared/SharedModule.java
@@ -1,21 +1,17 @@
-package com.segar.backend.tramites;
+package com.segar.backend.shared;
 
-import com.segar.backend.shared.SharedModule;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@ComponentScan(basePackages = "com.segar.backend.tramites")
-@EnableJpaRepositories(basePackages = "com.segar.backend.tramites.infrastructure")
+@ComponentScan(basePackages = "com.segar.backend.shared")
+@EnableJpaRepositories(basePackages = "com.segar.backend.shared.infrastructure")
 @EnableTransactionManagement
 @ConfigurationPropertiesScan
-@Import(SharedModule.class)
-public class TramitesModule {
+public class SharedModule {
 
-    public static final String MODULE_NAME = "tramites";
+    public static final String MODULE_NAME = "shared";
 }
-

35dc0bf32fef455fe99ff9dc3dd0158232b0e769
Daniel Terán <danielteran1310@gmail.com>
2025-09-24 23:04:22 -0500

    Remove implementation of TramiteService interface from TramiteServiceImpl class

--- segar-backend/src/main/java/com/segar/backend/tramites/service/TramiteServiceImpl.java
+++ segar-backend/src/main/java/com/segar/backend/tramites/service/TramiteServiceImpl.java
@@ -16,7 +16,6 @@ import com.segar.backend.tramites.api.dto.TimelineEventDTO;
 import com.segar.backend.tramites.api.dto.TrackingDTO;
 import com.segar.backend.tramites.infrastructure.*;
 
-import com.segar.backend.services.interfaces.TramiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
@@ -34,7 +33,7 @@ import java.util.List;
 import java.util.Locale;
 
 @Service
-public class TramiteServiceImpl implements TramiteService {
+public class TramiteServiceImpl{
 
     @Autowired
     private TramiteRepository tramiteRepo;

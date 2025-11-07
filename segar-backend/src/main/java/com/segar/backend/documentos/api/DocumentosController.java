e2fe067ee7735ab5676d09eb72522f42fb8db200
Daniel Terán <danielteran1310@gmail.com>
2025-11-07 17:02:09 -0500

    creacion logica de documentos

--- segar-backend/src/main/java/com/segar/backend/documentos/api/DocumentosController.java
+++ segar-backend/src/main/java/com/segar/backend/documentos/api/DocumentosController.java
@@ -1,6 +1,8 @@
 package com.segar.backend.documentos.api;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.TimeUnit;
@@ -8,6 +10,12 @@ import java.util.concurrent.TimeUnit;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.HttpMethod;
 import com.google.cloud.storage.Storage;
+import com.segar.backend.documentos.api.DocumentosController.SignedUrlRequest;
+import com.segar.backend.documentos.domain.Documento;
+import com.segar.backend.documentos.infrastructure.DocumentoRepository;
+import com.segar.backend.shared.domain.Tramite;
+import com.segar.backend.shared.infrastructure.TramiteRepository;
+
 import java.net.URL;
 
 
@@ -16,6 +24,12 @@ import java.net.URL;
 @RequestMapping("/api/documentos")
 @RequiredArgsConstructor
 public class DocumentosController {
+
+    @Autowired
+    private DocumentoRepository documentoRepository;
+
+    @Autowired
+    private TramiteRepository tramiteRepository;
 
     private final Storage storage;
 
@@ -31,6 +45,21 @@ public class DocumentosController {
                 Storage.SignUrlOption.httpMethod(HttpMethod.PUT),
                 Storage.SignUrlOption.withContentType()
         );
+
+        Documento documento = new Documento(request.getBucketName(), request.getObjectName(), request.getContentType());
+        String[] partes = request.getObjectName().split("/");
+
+        String nombreProducto = partes[1];
+
+        Tramite tramite = tramiteRepository.findByNombreProducto(nombreProducto).get();
+
+        System.out.println("Tramite encontrado: " + tramite.getId());
+        tramite.addDocumento(documento);
+        documento.setTramite(tramite);
+
+        tramiteRepository.save(tramite);
+        documentoRepository.save(documento);
+
 
         return signedUrl.toString();
     }
